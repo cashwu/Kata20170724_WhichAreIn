9c9b04ee268706083a07c6ea196dcf01c9f4644b
cash <cash@cashwu.com>
2017-07-23 22:17:20 +0800

    add second test case

--- Kata20170724_WhichAreIn/WhichAreInTests.cs
+++ Kata20170724_WhichAreIn/WhichAreInTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170724_WhichAreIn
@@ -14,6 +15,21 @@ namespace Kata20170724_WhichAreIn
             var a2 = new[] { "lively", "harp" };
             var expected = new string[] { };
 
+            AssertWhichAreInShouldBe(a1, a2, expected);
+        }
+
+        [TestMethod]
+        public void Input_s1_arp_s2_lively_harp_Should_return_empty()
+        {
+            var a1 = new[] { "arp" };
+            var a2 = new[] { "lively", "harp" };
+            var expected = new [] { "arp" };
+
+            AssertWhichAreInShouldBe(a1, a2, expected);
+        }
+
+        private static void AssertWhichAreInShouldBe(string[] a1, string[] a2, string[] expected)
+        {
             var whichAreIn = new WhichAreIn();
             var actual = whichAreIn.inArray(a1, a2);
             CollectionAssert.AreEqual(expected, actual);
@@ -24,7 +40,13 @@ namespace Kata20170724_WhichAreIn
     {
         public string[] inArray(string[] array1, string[] array2)
         {
-            return new string[] {};
+            var item = array1[0];
+            if (array2.Any(a => a.Contains(item)))
+            {
+                return array1;
+            }
+
+            return new string[]{};
         }
     }
 }

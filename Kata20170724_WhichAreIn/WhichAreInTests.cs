4227be71a537e1d4a3ddb97d989fa66578103737
cash <cash@cashwu.com>
2017-07-23 22:12:51 +0800

    first test

--- Kata20170724_WhichAreIn/WhichAreInTests.cs
+++ Kata20170724_WhichAreIn/WhichAreInTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170724_WhichAreIn
@@ -7,8 +8,23 @@ namespace Kata20170724_WhichAreIn
     public class WhichAreInTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_s1_bull_s2_lively_harp_Should_return_empty()
         {
+            var a1 = new[] { "bull" };
+            var a2 = new[] { "lively", "harp" };
+            var expected = new string[] { };
+
+            var whichAreIn = new WhichAreIn();
+            var actual = whichAreIn.inArray(a1, a2);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class WhichAreIn
+    {
+        public string[] inArray(string[] array1, string[] array2)
+        {
+            return new string[] {};
         }
     }
 }

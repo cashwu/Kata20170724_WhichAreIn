c33375d63b07f3c651075f5ed2bb022bb8c4a85f
cash <cash@cashwu.com>
2017-07-23 21:46:45 +0800

    init

--- Kata20170724_WhichAreIn/WhichAreInTests.cs
+++ Kata20170724_WhichAreIn/WhichAreInTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170724_WhichAreIn
 {
     [TestClass]
-    public class UnitTest1
+    public class WhichAreInTests
     {
         [TestMethod]
         public void TestMethod1()
